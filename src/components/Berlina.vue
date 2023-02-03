f7ee10e0a1fc6f82c60d10fa3e4dc1987042777f
msDiego <dmorrosilva@cifpfbmoll.eu>
2023-02-03 21:15:34 +0100

    WOP

--- src/components/Berlina.vue
+++ src/components/Berlina.vue
@@ -6,24 +6,28 @@ export default {
 
   data() {
     return {
-      berlinas: [],
-      menor: 999999,
-      mayor: 0,
+      data: {
+        berlinas: [],
+        mayor: 99999,
+        menor: 0,
+      }
+
     }
   },
   async mounted() {
     const response = await fetch(url);
-    this.berlinas = await response.json();
+    this.data.berlinas = await response.json();
 
-    this.berlinas.forEach(function (e) {
+    this.data.berlinas.forEach((value) => {
 
-      if (this.mayor > e.precio) {
-        this.mayor = e.precio
+      if (this.data.mayor > value.precio) {
+        this.data.mayor = value.precio
       }
-      if (this.menor < e.precio) {
-        this.menor = e.precio
+      if (this.data.menor < value.precio) {
+        this.data.menor = value.precio
       }
     })
+
   },
 
 
@@ -41,32 +45,29 @@ export default {
     </h1>
     <p class="px-12 py-4 text-xl">Nuestros vehículos:</p>
 
-    <div class="flex flex-row justify-evenly">
+    <div class="flex xl:flex-row justify-evenly lg:grid lg:grid-cols-3 lg:grid-rows-2">
 
-      <div class="flex flex-row justify-evenly mx-5 py-2">
-        <button class="border-b border-solid p-4 border-black hover:border-blue-500 duration-150">
-          ELÉCTRICOS
-        </button>
+      <div class="flex flex-row">
+        <div class="mx-5 py-2 lg:self-center">
+          <button class="border-b border-solid p-4 border-black hover:border-blue-500 duration-150">
+            ELÉCTRICOS
+          </button>
+        </div>
+
+        <div class="mx-5 py-2 lg:self-center">
+          <button class="border-b border-solid p-4 border-black hover:border-blue-500 duration-150">
+            MOTOR DE COMBUSTIÓN
+          </button>
+        </div>
+
+        <div id="slide" class="my-5 flex justify-center flex-row lg:self-center">
+          <p class="px-2">{{ data.menor }}</p>
+          <label class="flex align-middle">
+            <input type="range" v-bind:min="'{{menor}}'" v-bind:max="'{{mayor}}'" id="slider">
+          </label>
+          <p class="px-2">{{ data.mayor }}</p>
+        </div>
       </div>
-
-      <div class="flex flex-row justify-evenly mx-5 py-2">
-        <button class="border-b border-solid p-4 border-black hover:border-blue-500 duration-150">
-          MOTOR DE COMBUSTIÓN
-        </button>
-      </div>
-
-      <div id="slide" class="my-5 flex justify-center flex-row">
-        <p>{{ menor }}</p>
-        <label class="flex align-middle">
-          <input type="range" v-bind:min="'{{menor}}'" v-bind:max="'{{mayor}}'" id="slider">
-        </label>
-        <p>{{ mayor }}</p>
-      </div>
-
-    </div>
-
-
-    <div class="grid grid-cols-3 grid-rows-2">
 
 
     </div>
